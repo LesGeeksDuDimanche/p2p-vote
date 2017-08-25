159064ffc86cf2c221085a6849efa29662d5ab30
Chad Carbert <chadcarbert@me.com>
2017-08-25 23:15:18 +0200

    Add chartjs vue component and sample pie chart to Results

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,15 +1,42 @@
-x<template>
+<template>
   <!-- if you want automatic padding use "layout-padding" class -->
   <div class="layout-padding">
     <h1>Results</h1>
+    <div style="width: 250px;">
+      <pie-chart :data="chartData" :labels="chartLabels"></pie-chart>
+    </div>
   </div>
 </template>
 
 <script>
+import { Pie, mixins } from 'vue-chartjs'
+
+const PieChart = Pie.extend({
+  mixins: [mixins.reactiveProp],
+  props: ['data', 'labels'],
+  mounted () {
+    debugger;
+    this.renderChart({
+      labels: this.labels,
+      datasets: [
+        {
+          // hopefully we don't need more than this many colors...
+          backgroundColor: ["red", "blue", "green", "blue", "red", "blue"],
+          data: this.data
+        }
+      ]
+    })
+  }
+});
+
 export default {
   data () {
-    return {}
-  }
+    return {
+      chartData: [50, 50],
+      chartLabels: ['A', 'B']
+    }
+  },
+  components: { PieChart }
 }
 </script>
 
