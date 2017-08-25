52869b5ee683ae0f4cd65ab398dc710c0933bc14
Chad Carbert <chadcarbert@me.com>
2017-08-25 23:23:37 +0200

    Add totalVotes comptued property and better colors for piecart on Results

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -2,6 +2,7 @@
   <!-- if you want automatic padding use "layout-padding" class -->
   <div class="layout-padding">
     <h1>Results</h1>
+    Total Votes {{totalVotes}}
     <div style="width: 250px;">
       <pie-chart :data="chartData" :labels="chartLabels"></pie-chart>
     </div>
@@ -15,13 +16,12 @@ const PieChart = Pie.extend({
   mixins: [mixins.reactiveProp],
   props: ['data', 'labels'],
   mounted () {
-    debugger;
     this.renderChart({
       labels: this.labels,
       datasets: [
         {
           // hopefully we don't need more than this many colors...
-          backgroundColor: ["red", "blue", "green", "blue", "red", "blue"],
+          backgroundColor: ["#ee4035", "#f37737", "#f9e97a", "#7ac043", "#9ed9f7"],
           data: this.data
         }
       ]
@@ -34,6 +34,11 @@ export default {
     return {
       chartData: [50, 50],
       chartLabels: ['A', 'B']
+    }
+  },
+  computed: {
+    totalVotes: function() {
+      return this.chartData.reduce( ((sum, value) => sum + value) , 0);
     }
   },
   components: { PieChart }
