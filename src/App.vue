44d982dbc54c81dcc0c7b660b6dc89ead9dc9f9d
Chad Carbert <chadcarbert@me.com>
2017-08-26 08:39:01 +0200

    Put App in a pretty material card with max-width:

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,21 @@
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
   	<router-link to="/"><i>P2P Vote</i></router-link>
-  	<div class="text-center">
-    	<router-view />
+  	<div class="text-center" style="max-width: 750px; margin: auto;">
+      <q-card>
+    	  <router-view />
+      </q-card>
     </div>
   </div>
 </template>
 
 <script>
+import { QCard } from 'quasar'
 /*
  * Root component
  */
 export default {
-
-
-
+  components: { QCard }
 }
 </script>
 
